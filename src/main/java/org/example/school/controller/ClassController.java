fcc165a20318d04ccf2e9c6e246548a6f8c15501
maqingshan <cnmaqingshan@gmail.com>
2025-07-03 01:14:54 +0800

    修改

---
+++ src/main/java/org/example/school/controller/ClassController.java
@@ -0,0 +1,13 @@
+package org.example.school.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ClassController {
+
+    
+
+
+}
---
+++ src/main/java/org/example/school/model.java
@@ -0,0 +1,4 @@
+package org.example.school;
+
+public class model {
+}
